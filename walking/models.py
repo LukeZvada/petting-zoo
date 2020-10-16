6dbd5565dcce501e7a190d35c92f7e6ca2ef1ac8
LukeZvada <zvadamusic@gmail.com>
2020-10-16 12:32:53 -0700

    refactor init method again. Added __str__. Everything is working correctly

--- walking/models.py
+++ walking/models.py
@@ -1,41 +1,76 @@
 from datetime import date
 
 class Cougar:
-    def __init__(self, name, species, shift):
+    def __init__(self, name, species, shift, food):
         self.name = name
         self.species = species
         self.date_added = date.today()
-        self.slithering = True
+        self.walking = True
         self.shift = shift
+        self.food = food
+
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
+
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
 
 class Deer:
-    def __init__(self, name, species, shift):
+    def __init__(self, name, species, shift, food):
         self.name = name
         self.species = species
         self.date_added = date.today()
-        self.slithering = True
+        self.walking = True
         self.shift = shift
+        self.food = food
+
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
+
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
 
 class Bear:
-    def __init__(self, name, species, shift):
+    def __init__(self, name, species, shift, food):
         self.name = name
         self.species = species
         self.date_added = date.today()
-        self.slithering = True
+        self.walking = True
         self.shift = shift
+        self.food = food
+
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
+
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
 
 class Bigfoot:
-    def __init__(self, name, species, shift):
+    def __init__(self, name, species, shift, food):
         self.name = name
         self.species = species
         self.date_added = date.today()
-        self.slithering = True
+        self.walking = True
         self.shift = shift
+        self.food = food
+
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
+
+    def __str__(self):
+        return f"{self.name} is a {self.species}"    
 
 class Lion:
-    def __init__(self, name, species, shift):
+    def __init__(self, name, species, shift, food):
         self.name = name
         self.species = species
         self.date_added = date.today()
-        self.slithering = True
+        self.walking = True
         self.shift = shift
+        self.food = food
+
+    def feed(self):
+        print(f'{self.name} was fed {self.food} on {date.today().strftime("%m/%d/%Y")}')
+
+    def __str__(self):
+        return f"{self.name} is a {self.species}"
